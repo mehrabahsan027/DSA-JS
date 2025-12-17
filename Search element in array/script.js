8c32061b7fa6fcf3cc886687ce63478c8148dbf0
mehrabahsan027 <mehrabahsan027@gmail.com>
2025-12-17 16:38:14 +0600

    Finding the Index of an Element in an Array

---
+++ Search element in array/script.js
@@ -0,0 +1,29 @@
+//* Finding the Index of an Element in an Array
+
+// let array = [10, 20, 30, 40, 50, 60, 70, 80];
+// let element = 40;
+// let index = -1;  // Start with -1 to indicate "not found"
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === element) {
+//         index = i;
+//         break;
+//     }
+// }
+
+// console.log(index); 
+
+
+//* if duplicates exist
+
+// let array = [10, 20, 50, 40, 50, 60];
+// let element = 50;
+// let indices = [];
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === element) {
+//         indices.push(i);
+//     }
+// }
+
+// console.log(indices);
